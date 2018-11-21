d81ae2493d8727b298ab9d240c6f5477b1940d29
michal20-meet <michal20@meet.mit.edu>
2018-11-21 12:02:44 -0500

    lad.6

--- lab6.py
+++ lab6.py
@@ -1,17 +1,35 @@
 import turtle
 import random
-
 from turtle import Turtle
 
-class Square(Turtle):
-	def __init__(self, size, color):
+#1
+# turtle.colormode(255)
+
+# class Square(Turtle):
+# 	def __init__(self, size, color):
+# 		Turtle.__init__(self)
+# 		self.shapesize(size)
+# 		self.color(color)
+# 		self.shape("square")
+
+# 	def random_color(self):
+# 		self.color(random.randint(0,256), random.randint(0,256),random.randint(0,256))
+
+# s = Square(15,"yellow")
+# s.random_color()
+
+turtle.begin_poly()
+turtle.fd(100)
+turtle.left(20)
+turtle.fd(30)
+turtle.left(60)
+turtle.fd(50)
+turtle.end_poly()
+p = turtle.get_poly()
+
+class hexagon(Turtle):
+	def __init__(self, size):
 		Turtle.__init__(self)
 		self.shapesize(size)
-		self.color(color)
-		self.shape("square")
-
-	def random_color(self):
-		self.color(random)
-
-s = Square(40, "blue")
-s.random_color()
+		self.
+turtle.mainloop()
