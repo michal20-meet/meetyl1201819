c25b2158a50b17a11ef5ad58f8e39ccedc21c5f2
or20-meet <or20@meet.mit.edu>
2018-12-19 12:51:02 -0500

    lab8.py

--- lab8.py
+++ lab8.py
@@ -4,26 +4,36 @@ import random
 import math
 
 class ball(Turtle):
-	def __init__(self, radius, color, speed, d):
+	def __init__(self, radius, color, speed, dx, dy):
 		Turtle.__init__(self)
 		self.shape("circle")
 		self.shapesize(radius/10)
 		self.radius = radius
 		self.color(color)
 		self.speed(speed)
-		self.d = d
+		self.dx = dx
+		self.dy = dy
 
 	def move(self):
 		while True:
-			self.goto(self())
-			math.sqrt(math.pow((x2-x1),2) + math.pow((y2-y1),2))
+			self.pu()
+			self.goto(self.xcor() + self.dx, self.ycor() + self.dy)
 
-b1 = ball(25, "yellow", 10)
-b2 = ball(50, "green", 5)
+# D = math.sqrt(math.pow((x2-x1),2) + math.pow((y2-y1),2))
+
+
+b1 = ball(50, "yellow", 10, 1, 1)
+b2 = ball(25, "green", 5, 6, 2)
 
 b1.move()
 b2.move()
 
-#def check_collision(ball1, ball2):
+def check_collision(ball1, ball2):
+	# if ball1.pos() == ball2.pos():
+	# 	print("there's a collision")
+	if math.sqrt(math.pow((ball2.xcor()-ball1.xcor()),2) + math.pow((ball2.ycor()-ball1.ycor()),2)) <= ball1.radius + ball2.radius:
+		print("there is a collision")
+
+check_collision(b1, b2)
 
 turtle.mainloop()
